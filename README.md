7b179a252ec44e3c5a9563a78131dd5c11ae149f
Florian Dorn <florian.dorn@gmail.com>
2013-10-11 14:45:23 +0200

    updated README, moved loaders to ab.loader-object

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Benchmark your HTTP server – in your browser.
 
 ## Demo
 Check it out at [http://derflocki.github.io/ab-js](http://derflocki.github.io/ab-js/).
+
+## How it works
+Access to resouces via `XMLHttpRequest` and `<iframe>` is restricted by the [same-origin policy](http://en.wikipedia.org/wiki/Same-origin_policy). So, to get things done across domains **ab-js** uses the simple fact, that `<img>` is not!
+
+And since all of this is running in the Browser, benchmarking protected pages just works (after you've logged in of course).
--- src/ab.js
+++ src/ab.js
@@ -1,128 +1,123 @@
 "use strict";
-/**
- * load content in an iframe
- * @param params
- * @returns
- */
-function iFrameLoadeder (params) {
-	var now = new Date().getTime(), parser = document.createElement('a');
-	parser.href = params.url;
-	if((params.cache | false) == false) {
-		if(parser.search) {
-			params.url += "&_=" + now;
-		} else {
-			params.url += "?_=" + now;
-		}
-	}
-	
-	var data = {
-		requestStartTime : now,
-		requestEndTime : null,
-		requestDuration : null
-	}, deferred = $.Deferred(), iframe = $('<img style="display:none"></img>');
-	iframe.load(function () {
-		iframe.remove();
-		data.requestEndTime = new Date().getTime();
-		deferred.resolve(null, null, data);
-	}).error(function() {
-		iframe.remove();
-		data.requestEndTime = new Date().getTime();
-		deferred.resolve(null, null, data);
-	}) .attr({
-		"src" : params.url
-	});
-	//document.body.appendChild(iframe[0]);
-	$('body').append(iframe);
-	return deferred.promise();
-}
-/**
- * load with ajax
- * @param params
- * @returns
- */
-function ajaxLoader(params) {
-	return $.ajax({
-		method : params.get || 'get',
-		url : params.url,
-		cache : params.cache
-	});
-}
 
-var ab = function (url, requests, concurrency) {
-	var rc = 0;
-	var singleRequestComplete = function (data, textStatus, jqXHRIn) {
-		rc++;
-		var jqXHR = {};
-		jqXHR.requestStartTime = jqXHRIn.requestStartTime;
-		if(!jqXHR.requestEndTime) {
-			jqXHR.requestEndTime = new Date().getTime();
-		}
-		jqXHR.requestDuration = jqXHR.requestEndTime - jqXHR.requestStartTime;
-		
-		dfd.xhrs.push(jqXHR);
-		dfd.notify({
-			count : rc,
-			results : dfd.xhrs
-		});
-	};
-	var chunkComplete = function (data, textStatus, jqXHRIn) {
-		url_chunks.shift();
-		fetch_chunks(url_chunks);
-	};
-	var chunk = function (array, chunkSize) {
-		return [].concat.apply([], array.map(function (elem, i) {
-			return i % chunkSize ? [] : [ array.slice(i, i + chunkSize) ];
-		}));
-	};
-	var fetch_chunks = function (url_chunks) {
-		var i = 0, page, chunk = url_chunks[0], data = {}, deferreds = [];
-		if (chunk) {
-			// process this chunk
-			// after the chunk is processed remove the item from the
-			// array and call fetch_chunks again
-			for (i = 0; i < chunk.length; i++) {
-				page = iFrameLoadeder({
-					url : chunk[i],
-					cache: false
-				});
-				page.then(singleRequestComplete);
-				deferreds.push(page);
-			}
-			$.when.apply($, deferreds).then(chunkComplete);
+/**
+ * 
+ */
+var ab = function (url, requests, concurrency, post_data, loader_in) {
+		var loader = undefined;
+		if(!loader_in) {
+			loader = ab.loader["ajaxLoader"];
 		} else {
+			if(typeof loader_in !== 'function') {
+				loader = ab.loader[loader_in];
+				if(typeof loader !== 'function') {
+					throw new Error("Cannot use undeclared loader '" + loader_in + '". ');
+				}
+			} else {
+				loader = loader_in;
+			}
+		}
+		var method = "GET";
+		if(!post_data || post_data === "") {
+			post_data = undefined;
+		} else {
+			method = "POST";
+		}
+		try {
+			post_data = jQuery.parseJSON(post_data);
+		} catch (e) {
+			
+		}
+			
+		if(post_data)
+		//TODO: try the ajaxloader, then use the ImgLoader as a fallback
+		var rc = 0;
+		var singleRequestComplete = function (data, textStatus, jqXHRIn) {
+			rc++;
+			var jqXHR = {};
+			jqXHR.requestStartTime = jqXHRIn.requestStartTime;
+			if(!jqXHR.requestEndTime) {
+				jqXHR.requestEndTime = new Date().getTime();
+			}
+			jqXHR.requestDuration = jqXHR.requestEndTime - jqXHR.requestStartTime;
+			
+			dfd.xhrs.push(jqXHR);
+			dfd.notify({
+				count : rc,
+				results : dfd.xhrs
+			});
+		};
+		var chunkComplete = function (data, textStatus, jqXHRIn) {
+			url_chunks.shift();
+			fetch_chunks(url_chunks);
+		};
+		var chunk = function (array, chunkSize) {
+			return [].concat.apply([], array.map(function (elem, i) {
+				return i % chunkSize ? [] : [ array.slice(i, i + chunkSize) ];
+			}));
+		};
+		var fetch_chunks = function (url_chunks) {
+			var i = 0, page, chunk = url_chunks[0], data = {}, deferreds = [];
+			if (chunk) {
+				// process this chunk
+				// after the chunk is processed remove the item from the
+				// array and call fetch_chunks again
+				for (i = 0; i < chunk.length; i++) {
+					/*page = ImgLoadeder({
+						url : chunk[i],
+						cache: false
+					});
+					*/
+					page = loader({
+						url : chunk[i],
+						cache: false,
+						method: method,
+						data: post_data
+					});
+					if(page && page.then) {
+						page.then(singleRequestComplete);
+						deferreds.push(page);
+					} else {
+						dfd.reject(null);
+						throw new Error("'" + loader_in + '"does not return a jQuery Deferred.');
+					}
+					
+				}
+				$.when.apply($, deferreds).then(chunkComplete);
+			} else {
+				data['url'] = dfd.url;
+				data['requests'] = dfd.requests;
+				data['concurrency'] = dfd.concurrency;
+				data['results'] = dfd.xhrs;
+				dfd.resolve(data);
+			}
+		};
+		requests = parseInt(requests, 10);
+		concurrency = parseInt(concurrency, 10);
+		var dfd = new $.Deferred();
+		dfd.xhrs = [];
+		dfd.url = url;
+		dfd.requests = requests;
+		dfd.concurrency = concurrency;
+		
+		// make chunks of urls according to # of requests and concurrency
+		var urls = Array.apply(null, new Array(requests)).map(String.prototype.valueOf, url);
+		
+		var url_chunks = chunk(urls, concurrency);
+		fetch_chunks(url_chunks);
+		
+		var promise = dfd.promise();
+		promise.cancleNow = function () {
+			//just remove all remaining chunks
+			url_chunks = [];
+			var data = {};
 			data['url'] = dfd.url;
 			data['requests'] = dfd.requests;
 			data['concurrency'] = dfd.concurrency;
 			data['results'] = dfd.xhrs;
-			dfd.resolve(data);
-		}
-	};
-	requests = parseInt(requests, 10);
-	concurrency = parseInt(concurrency, 10);
-	var dfd = new $.Deferred();
-	dfd.xhrs = [];
-	dfd.url = url;
-	dfd.requests = requests;
-	dfd.concurrency = concurrency;
-	
-	// make chunks of urls according to # of requests and concurrency
-	var urls = Array.apply(null, new Array(requests)).map(String.prototype.valueOf, url);
-	
-	var url_chunks = chunk(urls, concurrency);
-	fetch_chunks(url_chunks);
-	
-	var promise = dfd.promise();
-	promise.cancleNow = function () {
-		//just remove all remaining chunks
-		url_chunks = [];
-		var data = {};
-		data['url'] = dfd.url;
-		data['requests'] = dfd.requests;
-		data['concurrency'] = dfd.concurrency;
-		data['results'] = dfd.xhrs;
-		dfd.reject(data);
-	};
-	return promise;
+			dfd.reject(data);
+		};
+		return promise;
 };
 
 function parseAbResults (results) {
@@ -155,3 +150,64 @@ function parseAbResults (results) {
 	
 	return total;
 };
+
+ab.loader = {
+	/**
+	 * <img> loader
+	 * 
+	 * same-origin policy does not apply
+	 * @param params
+	 * @returns
+	 */
+	 ImgLoader: function (params) {
+		var now = new Date().getTime(), parser = document.createElement('a');
+		parser.href = params.url;
+		if((params.cache | false) == false) {
+			if(parser.search) {
+				params.url += "&_=" + now;
+			} else {
+				params.url += "?_=" + now;
+			}
+		}
+		
+		var data = {
+			requestStartTime : now,
+			requestEndTime : null
+		}, deferred = $.Deferred(), callback = function() {
+			img.remove();
+			data.requestEndTime = new Date().getTime();
+			deferred.resolve(null, null, data);
+		}, img = $('<img style="display:none"></img>');
+		
+		img.load(callback).error(callback).attr({
+			"src" : params.url
+		});
+
+		$('body').append(img);
+		return deferred.promise();
+	},
+
+	/**
+	 * XMLHttpRequest loader
+	 * 
+	 * same-origin policy applies
+	 * @param params
+	 * @returns
+	 */
+	AjaxLoader: function(params) {
+		params.method = params.method || 'get';
+		var deferred = $.Deferred(), data = {
+			requestStartTime : new Date().getTime(),
+			requestEndTime : null,
+			status: null,
+			statusText: null
+		};
+		$.ajax(params).then(function(dataIn, textStatus, jqXHRIn) {
+			data.requestEndTime = new Date().getTime();
+			data.status = jqXHRIn.status;
+			data.statusText = jqXHRIn.statusText;
+			deferred.resolve(null, null, data);
+		});
+		return deferred.promise();
+	}
+}
